153fcbcc9249391f5409b9d3ea5bc06409580e9e
Alina <otelea.alina@yahoo.com>
2022-02-26 22:37:56 +0200

    changed composer

--- src/validate.php
+++ src/validate.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Validate;
+namespace Validate\Validate;
 
 class Validate {
 	public function validateData ($dataJson) {
@@ -8,57 +8,59 @@ class Validate {
 		$errors = [];
 		$isError = false;
 
-		if( 2 > strlen($dataJson['firstname'])) {
+		$data = json_decode($dataJson, true);
+
+		if( 2 > strlen($data['firstname'])) {
 			$isError = true;
 			array_push($errors,'Va rugam sa introduceti un nume valid');
 		}
 
-		if( 2 > strlen($dataJson['lastname'])) {
+		if( 2 > strlen($data['lastname'])) {
 			$isError = true;
 			array_push($errors,'Va rugam sa introduceti un prenume valid');
 		}
 
-		if( 2 > strlen($dataJson['company'])) {
+		if( 2 > strlen($data['company'])) {
 			$isError = true;
 			array_push($errors,'Va rugam sa introduceti un nume al companiei valid');
 		}
 
-		if( 2 > strlen($dataJson['street1'])) {
+		if( 2 > strlen($data['street1'])) {
 			$isError = true;
 			array_push($errors,'Va rugam sa introduceti o adresa valida');
 		}
 
-		if( 2 > strlen($dataJson['street2'])) {
+		if( 2 > strlen($data['street2'])) {
 			$isError = true;
 			array_push($errors,'Va rugam sa introduceti o adresa valida');
 		}
 
-		if( 2 > strlen($dataJson['city'])) {
+		if( 2 > strlen($data['city'])) {
 			$isError = true;
 			array_push($errors,'Va rugam sa introduceti un oras valid');
 		}
 
-		if( 2 > strlen($dataJson['county'])) {
+		if( 2 > strlen($data['county'])) {
 			$isError = true;
 			array_push($errors,'Va rugam sa introduceti un judet valid');
 		}
 
-		if( 2 > strlen($dataJson['zip']) && is_numeric((int)$dataJson['zip'])) {
+		if( 2 > strlen($data['zip']) && is_numeric((int)$data['zip'])) {
 			$isError = true;
 			array_push($errors,'Va rugam sa introduceti un zip code valid');
 		}
 
-		if( 2 > strlen($dataJson['country'])) {
+		if( 2 > strlen($data['country'])) {
 			$isError = true;
 			array_push($errors,'Va rugam sa introduceti o tara valida');
 		}
 
-		if( !preg_match("/^[0]{1}[7]{1}[0-9]{8}$/", $dataJson['phone'])) {
+		if( !preg_match("/^[0]{1}[7]{1}[0-9]{8}$/", $data['phone'])) {
 			$isError = true;
 			array_push($errors,'Va rugam sa introduceti un nr de telefon valid');
 		}
 
-		if( !filter_var($dataJson['email'], FILTER_VALIDATE_EMAIL)) {
+		if( !filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
 			$isError = true;
 			array_push($errors,'Va rugam sa introduceti un email valid');
 		}
@@ -66,22 +68,21 @@ class Validate {
 		if (true == $isError) {
 			var_dump($errors);
 		} else {
-			$name = $dataJson['firstname'] . ' ' . $dataJson['lastname'];
-			$fullAddress = 'Street: ' . $dataJson['street1'] . ' ' . $dataJson['street2'] . ',city: ' . 
-			$dataJson['city'] . ',county: '. 
-			 $dataJson['county'] . ', zip-code: ' . $dataJson['zip'] . ',country: ' . $dataJson['country']; 
+			$name = $data['firstname'] . ' ' . $data['lastname'];
+			$fullAddress = 'Street: ' . $data['street1'] . ' ' . $data['street2'] . ',city: ' . 
+			$data['city'] . ',county: '. 
+			 $data['county'] . ', zip-code: ' . $data['zip'] . ',country: ' . $data['country']; 
 
-			$ui = hash('sha256', $dataJson['phone'] . $dataJson['email']);
+			$ui = hash('sha256', $data['phone'] . $data['email']);
 
 
 			 echo '
-			 	<p>Name:'. $name .' </p>
-			 	<p>Company:'. $dataJson['company'] .' </p>
-			 	<p>FullAddress:'. $fullAddress .' </p>
-			 	<p>Phone:'. $dataJson['phone'] .' </p>
-			 	<p>Email:'. $dataJson['email'] .' </p>
-			 	<p>UI:'. $ui .' </p>
-			 ';
+			 	Name:'. $name .' 
+			 	Company:'. $data['company'] .' 
+			 	FullAddress:'. $fullAddress .' 
+			 	Phone:'. $data['phone'] .' 
+			 	Email:'. $data['email'] .' 
+			 	UI:'. $ui ;
 		}
 	}
 	}
